7856ad590ff01464379159aeacb7af2ad4ee27c6
Daifannual <daifannuramali@gmail.com>
2026-09-23 10:35:04 +0700

    feat: Percobaan 1 fix

---
+++ Pertemuan4/PemilihanIf06.java
@@ -0,0 +1,21 @@
+package Pertemuan4;
+
+import java.util.Scanner;
+
+/**
+ * PemilihanIf06
+ */
+public class PemilihanIf06 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = input.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
